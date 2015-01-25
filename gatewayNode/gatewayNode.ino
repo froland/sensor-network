517721f3ec820207d3260c1ad34c11a8cbe0c8d9
Francois ROLAND <fr.roland@gmail.com>
2015-01-25 20:55:06 +0100

    Rewritten roomNode code using the initial code from jeelab. The PIR used need PIR_PULLUP=0 and PIR_INVERTED=0.

--- gatewayNode/gatewayNode.ino
+++ gatewayNode/gatewayNode.ino
@@ -1,23 +1,47 @@
+/// @dir rf12serial
+/// Serial data over RF12 demo, works in both directions.
+// 2009-04-17 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+
 #include <JeeLib.h>
-#include "dataFormat.h"
+#include <RF12sio.h>
+
+RF12 RF12;
+
+struct Payload {
+  byte light;     // light sensor: 0..255
+  byte moved :1;  // motion detector: 0..1
+  byte humi  :7;  // humidity: 0..100
+  int temp   :10; // temperature: -500..+500 (tenths)
+  byte lobat :1;  // supply voltage dropped under 3.1V: 0..1
+};
+Payload payload;
 
 void setup() {
   Serial.begin(9600);
-  rf12_configSilent();
+  Serial.print("\n[rf12serial]");
+  rf12_config();
 }
 
 void loop() {
-  if (rf12_recvDone() && rf12_crc == 0 && rf12_len == sizeof (Measure)) {
-    Measure measure = *(Measure*) rf12_data;
-    Serial.print(measure.nodeId, DEC);
-    Serial.print(';');
-    Serial.print(measure.temperature, DEC);
-    Serial.print(';');
-    Serial.print(measure.humidity, DEC);
-    Serial.print(';');
-    Serial.print(measure.brightness, DEC);
-    Serial.print(';');
-    Serial.print(measure.presence, DEC);
-    Serial.println();
+  if (rf12_recvDone() && rf12_crc == 0) {
+    if (rf12_len == sizeof (Payload)) {
+      payload = *(Payload*) rf12_data;
+      int nodeId = RF12_HDR_MASK & rf12_hdr;
+      Serial.print(nodeId, DEC);
+      Serial.print(": light=");
+      Serial.print(payload.light, DEC);
+      Serial.print(", moved:");
+      Serial.print(payload.moved, DEC);
+      Serial.print(", humi:");
+      Serial.print(payload.humi, DEC);
+      Serial.print(", temp:");
+      Serial.print(payload.temp / 10.0, 1);
+      Serial.print(", lobat:");
+      Serial.print(payload.lobat, DEC);
+      Serial.println();
+    }
+    if (RF12_WANTS_ACK) {
+      rf12_sendStart(RF12_ACK_REPLY, 0, 0);
+    }
   }
 }
